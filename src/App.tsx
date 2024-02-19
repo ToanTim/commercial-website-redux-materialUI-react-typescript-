df727c889059a3340037d4bac00c38df2c2675ec
rrtotr <toan.tran@tuni.fi>
2024-02-19 15:37:09 +0700

    add file strcucture and react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen";
+import ProductScreen from "./pages/ProductScreen";
+import { NoPageScreen } from "./pages/NoPageScreen";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+
+        <Route path="products" element={<ProductScreen />} />
+        <Route path="*" element={<NoPageScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/NoPageScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NoPageScreen = () => {
+  return (
+    <div>NoPageScreen</div>
+  )
+}
