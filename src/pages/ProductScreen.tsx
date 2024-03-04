33403ed19b4ac266c600912020c4a62643e45f48
rrtotr <toan.tran@tuni.fi>
2024-03-05 00:18:26 +0200

    add code

--- src/pages/ProductScreen.tsx
+++ src/pages/ProductScreen.tsx
@@ -29,28 +29,23 @@ const ProductScreen = () => {
   //TODO: challenge
   //wwhen we display category: what should we do when that categories does have any product yet?
   // Implement Pagination so it can be reuseable
+  // Add loadingScreen logic
 
   const navigate = useNavigate();
-  // Dummy categories array (replace with your actual categories array)
   const categoryState = useAppSelector((state: RootState) => state.categories);
   const filteredProducts = useAppSelector(
     (state: RootState) => state.products.entityByCategory
   );
-
   const { entityCategory } = categoryState;
   const { search } = useLocation();
   const queryParams = new URLSearchParams(search);
   const dispatch = useAppDispatch();
-  //-1 cause pagination starts with 1 but array start with 0
-  let page = parseInt(queryParams.get("page") ?? "", 0) - 1;
+
+  //pages variables
+  let page = parseInt(queryParams.get("page") ?? "", 0) - 1; // //-1 cause pagination starts with 1 but array start with 0
   let startIndex = 10 * page;
   let endIndex = startIndex + 10;
-
-  console.log("filteredProducts", filteredProducts);
-
   const totalPages = Math.ceil(Object.keys(filteredProducts).length / 10);
-
-  console.log("totalPages", totalPages);
 
   //reducer to filter product by categories
   useReduxReducerRunner(
@@ -58,19 +53,16 @@ const ProductScreen = () => {
     [entityCategory],
     entityCategory
   );
-  /* useEffect(() => {
-    dispatch(filterProductsByCategories(entityCategory));
-  }, [dispatch, entityCategory]); */
 
-  //if page more than total pages
   if (page > totalPages) {
+    //if page more than total pages
     page = totalPages;
     startIndex = totalPages * 10;
     endIndex = Object.keys(filteredProducts).length;
   }
 
-  //if endIndex is more than array length
   if (endIndex > Object.keys(filteredProducts).length) {
+    //if endIndex is more than array length
     endIndex = Object.keys(filteredProducts).length;
   }
 
@@ -88,7 +80,7 @@ const ProductScreen = () => {
 
   const handleChangePagination = (
     event: React.ChangeEvent<unknown>,
-    value: number //this is numebr of page clicked on pagition bar
+    value: number //this is numeber of page clicked on pagition bar
   ) => {
     navigate(websiteRouterList.product.shortLink + value);
     scrollToTop();
@@ -110,8 +102,6 @@ const ProductScreen = () => {
     </Box>
   );
 
-  /* console.log("entityCategory", entityCategory); */
-  /* console.log("filteredProducts", filteredProducts); */
   return (
     <>
       <CssBaseline />
@@ -126,15 +116,36 @@ const ProductScreen = () => {
             ) {
               return (
                 <div key={category.id}>
-                  <Typography variant="h5" gutterBottom marginTop={10}>
-                    {category.name}
-                  </Typography>
-                  {/* Render products for each category */}
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      marginBottom: "10px",
+                      marginTop: "50px",
+                    }}
+                  >
+                    <Typography variant="h5" gutterBottom>
+                      {category.name}
+                    </Typography>
+                    {/* "See More" button */}
+
+                    {filteredProducts[category.id].length > 10 ? (
+                      <Button variant="contained" color="primary" size="small">
+                        See More
+                      </Button>
+                    ) : (
+                      <></>
+                    )}
+                  </Box>
                   <Box sx={{ display: "flex", overflowX: "auto", gap: 10 }}>
                     {/* Dummy products (replace with your actual products data) */}
-                    {filteredProducts[category.id].map((item) => (
+                    {filteredProducts[category.id].slice(0, 10).map((item) => (
                       <Grid item xs={12} sm={6} md={4}>
-                        <Card className="card">
+                        <Card
+                          className="card"
+                          sx={{ maxWidth: 345, minWidth: 250 }}
+                        >
                           <CardMedia
                             component="img"
                             height="140"
@@ -152,7 +163,16 @@ const ProductScreen = () => {
                             <Typography variant="body2" color="text.secondary">
                               {item.description}
                             </Typography>
+                            <Typography
+                              variant="h6"
+                              color="text.secondary"
+                              align="justify"
+                              style={{ textAlign: "center" }}
+                            >
+                              Price: {item.price} $
+                            </Typography>
                           </CardContent>
+
                           <CardActions>
                             <Button size="small">More detail</Button>
                             <Button size="small">Add to my cart</Button>
